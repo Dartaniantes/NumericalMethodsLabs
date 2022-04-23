4d02358b77a3fcc9c168ba7b6de595c5730639bb
Demiani <demkafrog@gmail.com>
2022-04-23 21:46:12 +0200

    Newton method added. Simple iterations output now shows main function, fi, dfi

---
+++ NewtonMethod.cs
@@ -0,0 +1,207 @@
+﻿using System.Numerics;
+
+namespace NumericalMethodsLabs
+{
+    public class NewtonMethod
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть з якою точнiстю розрахувати резутьтат");
+            double Eps = Double.Parse(Console.ReadLine());
+
+            //double Eps = 0.0000001;
+            Console.WriteLine("Введiть реальну частину комплексного числа");
+
+            double rel = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введiть уявну частину комплексного числа");
+
+            double img = Double.Parse(Console.ReadLine());
+
+            //double rel = -2;
+            //double img = 0;
+
+            Complex st = new Complex(rel, img);
+
+            int coun = 0;
+
+            Complex Nuton11(Complex start)
+            {
+                Complex del = new Complex(1, 0);
+                while (Math.Abs(del.Real) > Eps)
+                {
+                    coun++;
+                    del = Pohidna(0, start) / Pohidna(1, start);
+                    start -= del;
+                }
+                return start;
+            }
+
+            Complex u = Pohidna(1, st);
+
+            int coun2 = 0;
+            Complex Nuton12(Complex start)
+            {
+                Complex del = new Complex(1, 0);
+                while (Math.Abs(del.Real) > Eps)
+                {
+                    coun2++;
+
+                    del = Pohidna(0, start) / u;
+                    start -= del;
+                }
+                return start;
+            }
+            Console.WriteLine();
+            Console.WriteLine("МЕТОД НЬЮТОНА");
+            Console.WriteLine("Корiнь = " + Nuton11(st));
+            Console.WriteLine("Значення = " + Pohidna(0, Nuton11(st)));
+            Console.WriteLine(coun + " операцiй");
+
+
+            Console.WriteLine();
+            Console.WriteLine("СПРОЩЕНИЙ МЕТОД НЬЮТОНА");
+            Console.WriteLine("Корiнь = " + Nuton12(st));
+            Console.WriteLine("Значення = " + Pohidna(0, Nuton12(st)));
+            Console.WriteLine(coun2 + " операцiй");
+
+            //этот метод мспользуеться в ньютоновских методах
+
+            Complex Pohidna(int index, Complex val)
+            {
+                //x ^ 6 - 3x ^ 2 + x - 1
+                switch (index)
+                {
+                    case 0:
+                        return Complex.Pow(val, 6) - (3 * Complex.Pow(val, 2)) + val - 1;
+                    case 1:
+                        return 6 * Complex.Pow(val, 5) - (3 * 2 * val) + 1;
+                    case 2:
+                        return 6 * 5 * Complex.Pow(val, 4) - 6;
+                    case 3:
+                        return 6 * 5 * 4 * Complex.Pow(val, 3);
+
+                    case 4:
+                        return 6 * 5 * 4 * 3 * Complex.Pow(val, 2);
+                    case 5:
+                        return 6 * 5 * 4 * 3 * 2 * val;
+                }
+                return 6 * 5 * 4 * 3 * 2;
+
+                //tg(0,58x+0,1)-x^2=0
+                //switch (index)
+                //{
+                //    case 0:
+                //        return Complex.Tan(((0.58 * val) + 0.1)) - Complex.Pow(val, 2);
+                //    case 1:
+                //        return 0.58 * (Complex.Pow(Complex.Tan(((0.58 * val) + 0.1)), 2)) - 2 * val + 0.57;
+
+                //}
+                //return 0.58 * (Complex.Pow(Complex.Tan(((0.58 * val) + 0.1)), 2)) - 2 * val + 0.57;
+
+            }
+            Complex Iret(Complex z)
+            {
+                if (DifLChange(0, z).Real < 1)
+                {
+                    Complex d = new Complex(1, 0);
+                    while (Math.Abs(d.Real) > Eps)
+                    {
+                        d = z - LChange(0, z);
+                        z = LChange(0, z);
+                    }
+                }
+                else
+                {
+                    Complex d = new Complex(1, 0);
+                    Complex diff = -21;
+                    //-10
+                    while (Math.Abs(d.Real) > Eps)
+                    {
+                        d = l(z) / diff;
+                        z = z - d;
+                    }
+                    return z;
+                }
+                return z;
+            }
+
+            Complex Iret2(Complex z)
+            {
+                if (DifLChange(1, z).Real < 1)
+                {
+                    Complex d = new Complex(1, 0);
+                    while (Math.Abs(d.Real) > Eps)
+                    {
+                        d = z - LChange(1, z);
+                        z = LChange(1, z);
+                    }
+                }
+                else
+                {
+                    Complex d = new Complex(1, 0);
+                    Complex diff = -5;
+                    //-10
+                    while (Math.Abs(d.Real) > Eps)
+                    {
+                        d = l(z) / diff;
+                        z = z - d;
+                    }
+                    return z;
+                }
+                return z;
+            }
+
+            Complex l(Complex val)
+            {
+                //2x^3-3x^2-12x+8=0
+                return 2 * Complex.Pow(val, 3) - (3 * Complex.Pow(val, 2)) - (12 * val) + 8;
+            }
+
+            Complex DifLChange(int index, Complex val)
+            {
+                //x^2/2-x/2
+
+                switch (index)
+                {
+                    case 0:
+                        //x^3/6-x^2/4+8/12
+                        return Complex.Pow(val, 2) / 2 - val / 2;
+
+                    case 1:
+                        //
+                        return (Complex.Pow(val, 2) - 2) / (Complex.Pow(2.0 / 3.0 * Complex.Pow(val, 3) - 4 * val + 8.0 / 3, 1.0 / 2.0));
+
+
+                }
+                return Complex.Pow(val, 2) / 2 - val / 2;
+            }
+            Complex LChange(int index, Complex val)
+            {
+
+                switch (index)
+                {
+                    case 0:
+                        //x^3/6-x^2/4+8/12
+
+                        return Complex.Pow(val, 3) / 6 - (Complex.Pow(val, 2) / 4) + 8.0 / 12.0;
+
+                    case 1:
+                        //
+                        return Complex.Pow(2.0 / 3.0 * Complex.Pow(val, 3) - 4 * val + 8.0 / 3, 1.0 / 2.0);
+
+                }
+                return Complex.Pow(val, 3) / 6 - (Complex.Pow(val, 2) / 4) + 8.0 / 12.0;
+            }
+            //Console.WriteLine();
+            //Console.WriteLine("МЕТОД ПРОСТОЇ IТЕРАЦIЇ");
+            //Console.WriteLine("Корiнь = " + Iret(st));
+            //Console.WriteLine("Значення = " + l(Iret(st)));
+            //Console.WriteLine();
+            //Console.WriteLine("МЕТОД ПРОСТОЇ IТЕРАЦIЇ");
+            //Console.WriteLine("Корiнь = " + Iret2(st));
+            //Console.WriteLine("Значення = " + l(Iret2(st)));
+        }
+    }
+}
+
--- SimpleIterationsMethod.cs
+++ SimpleIterationsMethod.cs
@@ -2,7 +2,7 @@
 
 namespace NumericalMethodsLabs
 {
-    class Lab2NewtonAndSimpleIteration
+    class SimpleIterationsMethod
     {
         public delegate double Func(double x);
         static int iterations = 0;
@@ -11,31 +11,42 @@ namespace NumericalMethodsLabs
         public static void Main(string[] args)
         {
             p();
-            q();
+            q();    
         }
 
         public static void q()
         {
             Console.WriteLine("\n\tq(x) root finding started\n");
 
-            Func q = x => 2*Pow(x,3) - 3 * Pow(x, 2) -12*x + 8;             // roots: 0.611315455 , -2.1519448 , 3.0406293
+            Func q = x => 2 * Pow(x, 3) - 3 * Pow(x, 2) - 12 * x + 8;             // roots: 0.611315455 , -2.1519448 , 3.0406293
+            Console.WriteLine("q(x) = 2x^3 - 3x^2 - 12x + 8");
 
             //for root : x = 0.611315455
             //dFi(x01)<1 when x01=[-0.9 ; 1.9]
             Func fi = x => (2 * Pow(x, 3) - 3 * Pow(x, 2) + 8) / 12;          
             Func dFi = x => (Pow(x, 2) - x) / 2;
-            double x01 = 1;                            
+            double x01 = 1;
+
+            Console.WriteLine("fi(x) = (2x^3 - 3x^2 + 8)/12");
+            Console.WriteLine("dfi(x) = (x^2-x)/2");
+            Console.WriteLine("|dfi(" + x01 + ")| = " + Abs(dFi(x01)));
 
             findRoot(q, fi, dFi, x01);
 
             // for roots : x = 3.0406293  x = -2.15194; 
             //dfi2(x02) < 1 when x02 = (-inf;-5.26] + [-4.14;0.14] + [1.253;+inf]
-            Func fi2 = x => Cbrt((3 * Pow(x, 2) + 12 * x - 8) / 2);                                 //
+            Func fi2 = x => Cbrt((3 * Pow(x, 2) + 12 * x - 8) / 2);                                 
             Func dFi2 = x => (x + 2)*Cbrt(Pow(2,2)) / Cbrt(Pow(3 * Pow(x, 2) + 12 * x - 8, 2));
-            double x02 =0.14;
+            double x02 = 0.14;
+
+            Console.WriteLine("fi2(x) = Cbrt((3x^2 + 12x - 8)/2)");
+            Console.WriteLine("dfi2(x) = Cbrt(3x^2 +12x - 8)/2");
+            Console.WriteLine("|dfi2(" + x02 + ")| = " + Abs(dFi2(x02)));
             findRoot(q, fi2, dFi2, x02);
 
             double x03 = 1.5;
+
+            Console.WriteLine("|dfi2(" + x03 + ")| = " + Abs(dFi(x03)));
             findRoot(q, fi2, dFi2, x03);
 
             Console.WriteLine("\n\tq(x) root finding ended");
@@ -54,19 +65,22 @@ namespace NumericalMethodsLabs
             result = SimpleIterations(fi, x0, eps);
             if (Abs(Round(result)%result) <= eps)
                 result = Round(result);
-            Console.WriteLine("\nx = " + result + "\t\t x0 = "+ x0 );
-            Console.WriteLine("iteratinos =  " + iterations + "\n");
+            Console.WriteLine("root : x = " + result + "\t\t x0 = "+ x0 );
+            Console.WriteLine("operations =  " + iterations + "\n");
         }
 
         public static void p()
         {
             Console.WriteLine("\tp(x) root finding started\n");
             Func p = x => Pow(x, 2) - Sin(PI * x);          // roots : x = 0 ;  x = 0.78723712
+            Console.WriteLine("p(x) = x^2 - sin(PI*x)");
 
             //for root 0;   dFi(x01) < 1 when x01=[-0.9;0.9]
             Func fi = x => Asin(Pow(x, 2)) / PI;
             Func dFi = x => 2 * x / (PI * Sqrt(1 - Pow(x, 4)));
             double x01 = 0.9;
+            Console.WriteLine("fi(x) = asin(x^2) / PI");
+            Console.WriteLine("dfi(x) = 2x/(PI * sqrt(1-x^4))");
             Console.WriteLine("|dfi(" + x01 + ")| = " + Abs(dFi(x01)));
 
             findRoot(p, fi, dFi, x01);
@@ -74,8 +88,10 @@ namespace NumericalMethodsLabs
             //for root 0.78723712;   dFi2(x02) < 1 when x02={... -1.5; 0.5; 2.5 ...}+-0.19
             Func fi2 = x => Sqrt(Sin(PI * x));
             Func dFi2 = x => PI * Cos(PI * x) / (2 * Sqrt(Sin(PI * x)));
-            double x02 = 0.5+0.19;
-            Console.WriteLine("|dfi("+x02+")| = " + Abs(dFi2(x02)));
+            double x02 = 0.5 + 0.19;
+            Console.WriteLine("fi2(x) = sqrt(sin(PI*x))");
+            Console.WriteLine("dfi2(x) = PI * cos(PI * x) / (2*sqrt(sin(PI*x))5");
+            Console.WriteLine("|dfi2("+x02+")| = " + Abs(dFi2(x02)));
 
             findRoot(p, fi2, dFi2, x02);
             Console.WriteLine("\tp(x) root finding ended\n");
